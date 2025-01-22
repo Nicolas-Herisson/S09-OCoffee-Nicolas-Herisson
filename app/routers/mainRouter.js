57707aec0b6732662f072603e8fa881b508fa39a
Nicolas Herisson <nicolas.herisson77@gmail.com>
2025-01-22 17:17:19 +0100

    global upgrade

--- app/routers/mainRouter.js
+++ app/routers/mainRouter.js
@@ -5,6 +5,10 @@ const mainRouter = express.Router();
 
 mainRouter.get("/", mainController.showHomepage);
 mainRouter.get("/catalogue", mainController.showCatalogpage);
+// mainRouter.get("/catalogue/origin", mainController.showOriginpage);
+// mainRouter.get("/catalogue/spec", mainController.showSpecpage);
+// mainRouter.get("/catalogue/price", mainController.showPricepage);
 mainRouter.get("/catalogue/:id", mainController.showDetailpage);
 
 export default mainRouter;
+
