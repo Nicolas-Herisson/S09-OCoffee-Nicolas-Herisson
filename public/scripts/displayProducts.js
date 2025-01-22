2f02b0b98c41fbd23310404704c86f6b326c0a60
Nicolas Herisson <nicolas.herisson77@gmail.com>
2025-01-23 00:02:33 +0100

    form css | form upgrade

--- public/scripts/displayProducts.js
+++ public/scripts/displayProducts.js
@@ -2,44 +2,40 @@ const products = document.querySelectorAll('.product.container');
 const coffeesButton = document.querySelector('.products.button');
 let open = false;
 let index = 0;
-const url = window.location.href;
 
-// if on catalogue/:id(details) or filtered result = 1
-if (!isNaN(url.split('/').pop()) || products.length === 1)
-{
-    products[0].classList.add('active');
+
+//displayed 3 max at init
+for (let i = 0; i < 3; i++) {
+    if (products[i])
+        products[i].classList.add('active');
 }
-else
-{
-    products[0].classList.add('active');
-    products[1].classList.add('active');
-    products[2].classList.add('active');
 
+if (coffeesButton)
+{
     coffeesButton.addEventListener("click", () => {
 
         if (open)
         {
+            //close & init
             products.forEach(product => {
 
                 if (index > 2)
-                {
                     product.classList.toggle('active');
-                    open = false;
-                    coffeesButton.innerHTML = "Voir tous les cafés"
-                }
+
                 index++;
             });
+            open = false;
+            coffeesButton.innerHTML = "Voir tous les cafés"
         }
         else if (!open)
+        {
             products.forEach(product => {
                 if (!product.classList.contains('active'))
-                {
                     product.classList.toggle('active');
-                    open = true;
-                    coffeesButton.innerHTML = "Cacher tous les cafés"
-                }
-        });
-        index = 0;
-    })
-}
 
+            });
+            open = true;
+            coffeesButton.innerHTML = "Cacher tous les cafés"
+        }
+    });
+}
